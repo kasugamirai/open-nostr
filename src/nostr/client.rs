61548de62862dbe75013b48d61ccc2957f44ac7d
ungigdu <ungigdu@protonmail.com>
2024-05-08 02:34:54 +0900

    fix: revert to avoid thread-safe client issue

--- src/nostr/client.rs
+++ src/nostr/client.rs
@@ -1,8 +1,5 @@
 use nostr_sdk::prelude::*;
 use crate::nostr::register::*;
-use std::sync::Arc;
-use tokio::task;
-use tokio::sync::mpsc::Receiver;
 
 pub enum WorkerCommand {
     Subscribe {
@@ -13,23 +10,16 @@ pub enum WorkerCommand {
     Start,
 }
 
-pub async fn client_worker(client: Client, client_key: &str, mut command_rx: Receiver<WorkerCommand>) {
-    let client_arc = Arc::new(client);
-
+pub async fn client_worker(client: Client, client_key: &str, mut command_rx: tokio::sync::mpsc::Receiver<WorkerCommand>) {
     while let Some(command) = command_rx.recv().await {
         match command {
             WorkerCommand::Subscribe { filters, handler } => {
-                let client_clone = client_arc.clone();
-                let client_key_clone = client_key.to_string();
-                task::spawn(async move {
-                    let _ = SUB_REGISTER.add_subscription(&client_clone, &client_key_clone, None, filters, handler, None).await;
-                });
+                // Handle the subscription using the handler
+                let _ = SUB_REGISTER.add_subscription(&client, client_key, None, filters, handler, None).await;
+                
             }
             WorkerCommand::Start => {
-                let client_clone = client_arc.clone();
-                task::spawn(async move {
-                    SUB_REGISTER.handle_notifications(&client_clone).await.unwrap();
-                });
+                SUB_REGISTER.handle_notifications(&client).await.unwrap();
             },
             // Handle other commands here
         }
