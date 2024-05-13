49ef939cce63e89747c3e9d93a1e75364d9de2b9
ahriknow <ahriknow@ahriknow.com>
2024-05-13 15:32:58 +0800

    feat: capybastr

--- src/init/mod.rs
+++ src/init/mod.rs
@@ -5,9 +5,44 @@ use nostr_indexeddb::WebDatabase;
 use nostr_sdk::{Client, ClientBuilder, RelayMessage, RelayPoolNotification, SubscriptionId};
 use serde::ser::StdError;
 
-use crate::{nostr::register::*, Route};
-use crate::store::subscription::CustomSub;
+use crate::store::subscription::{CustomSub, RelaySet, SubNames};
 use crate::store::CBWebDatabase;
+use crate::{nostr::register::*, Route};
+
+async fn init() {
+    let database = CBWebDatabase::open("Capybastr-db").await.unwrap();
+
+    let name_list = SubNames::new(vec!["Dog".to_string(), "Car".to_string()]);
+    database.save_sub_name_list(name_list).await.unwrap();
+
+    let sub = CustomSub::default_with_opt(
+        "Dog".to_string(),
+        "wss://relay.damus.io".to_string(),
+        vec!["dog".to_string()],
+        true,
+    );
+    database.save_custom_sub(sub).await.unwrap();
+
+    let sub = CustomSub::default_with_opt(
+        "Car".to_string(),
+        "wss://btc.klendazu.com".to_string(),
+        vec!["car".to_string()],
+        false,
+    );
+    database.save_custom_sub(sub).await.unwrap();
+
+    let rs = RelaySet {
+        name: "Damus".to_string(),
+        relays: vec!["wss://relay.damus.io".to_string()],
+    };
+    database.save_relay_set(rs).await.unwrap();
+
+    let rs = RelaySet {
+        name: "Klendazu".to_string(),
+        relays: vec!["wss://btc.klendazu.com".to_string()],
+    };
+    database.save_relay_set(rs).await.unwrap();
+}
 
 #[allow(non_snake_case)]
 pub fn App() -> Element {
@@ -26,79 +61,71 @@ pub fn App() -> Element {
     // hook: on mounted
     let on_mounted = move |_| {
         spawn(async move {
-            let CBdatabase = CBWebDatabase::open("Capybastr-db").await.unwrap();
+            // TODO: init().await;
 
+            let database = CBWebDatabase::open("Capybastr-db").await.unwrap();
 
-            // let sub_names: Vec<String> = vec!["Dog".to_string(), "Car".to_string()];
+            let name_list = database.get_sub_name_list().await.unwrap();
 
-            // let mut subs = vec![];
-            // for i in sub_names.iter() {
-            //     subs.push(CustomSub::from(i));
-            //     // match db2.read_data::<String>(i).await {
-            //     //     Ok(v) => subs.push(CustomSub::from(&v)),
-            //     //     Err(e) => eprintln!("Error reading data: {}", e),
-            //     // }
-            //     // match db.read_data::<String>(i).await {
-            //     //     Ok(v) => subs.push(CustomSub::from(&v)),
-            //     //     Err(e) => eprintln!("Error reading data: {}", e),
-            //     // }
-            // }
-            // async fn handler_text_note(
-            //     notification: RelayPoolNotification,
-            // ) -> Result<bool, Box<dyn StdError>> {
-            //     if let RelayPoolNotification::Message {
-            //         message: RelayMessage::Event { event, .. },
-            //         ..
-            //     } = notification
-            //     {
-            //         println!("TextNote: {:?}", event);
-            //         tracing::info!("TextNote: {:?}", event);
-            //     }
-            //     Ok(false)
-            // }
+            let mut subs = vec![];
+            for i in name_list.names.iter() {
+                let sub = database.get_custom_sub(i.to_string()).await.unwrap();
+                subs.push(sub);
+            }
 
-            // let mut cs = clients.write();
-            // for i in subs.iter() {
-            //     let client_builder =
-            //         ClientBuilder::new().database(WebDatabase::open("EVENTS_DB").await.unwrap());
-            //     let c = client_builder.build();
-            //     c.add_relays(i.relay_set.relays.clone()).await.unwrap();
-            //     c.connect().await;
-            //     cs.insert(i.name.clone(), c.clone());
+            async fn handler_text_note(
+                notification: RelayPoolNotification,
+            ) -> Result<bool, Box<dyn StdError>> {
+                if let RelayPoolNotification::Message {
+                    message: RelayMessage::Event { event, .. },
+                    ..
+                } = notification
+                {
+                    println!("TextNote: {:?}", event);
+                    tracing::info!("TextNote: {:?}", event);
+                }
+                Ok(false)
+            }
 
-            //     if i.live {
-            //         let s = i.clone();
-            //         use_coroutine(|_: UnboundedReceiver<()>| async move {
-            //             (*register.read())
-            //                 .add_subscription(
-            //                     &c.clone(),
-            //                     SubscriptionId::new(s.name.clone()),
-            //                     s.get_filters(),
-            //                     Arc::new(|notification| Box::pin(handler_text_note(notification))),
-            //                     None,
-            //                 )
-            //                 .await
-            //                 .unwrap();
-            //             (*register.read())
-            //                 .handle_notifications(&c.clone())
-            //                 .await
-            //                 .unwrap();
-            //         });
-            //     }
-            // }
+            let mut cs = clients.write();
+            for i in subs.iter() {
+                let client_builder =
+                    ClientBuilder::new().database(WebDatabase::open("EVENTS_DB").await.unwrap());
+                let c = client_builder.build();
+                c.add_relays(i.relay_set.relays.clone()).await.unwrap();
+                c.connect().await;
+                cs.insert(i.name.clone(), c.clone());
 
-            // for i in subs.iter() {
-            //     all_sub.push(i.clone());
-            // }
+                if i.live {
+                    let s = i.clone();
+                    use_coroutine(|_: UnboundedReceiver<()>| async move {
+                        (*register.read())
+                            .add_subscription(
+                                &c.clone(),
+                                SubscriptionId::new(s.name.clone()),
+                                s.get_filters(),
+                                Arc::new(|notification| Box::pin(handler_text_note(notification))),
+                                None,
+                            )
+                            .await
+                            .unwrap();
+                        (*register.read())
+                            .handle_notifications(&c.clone())
+                            .await
+                            .unwrap();
+                    });
+                }
+            }
+
+            for i in subs.iter() {
+                all_sub.push(i.clone());
+            }
         });
     };
 
     let mut root_click_pos = use_context_provider(|| Signal::new((0.0, 0.0)));
 
-    let style = format!(
-        "\n{}",
-        include_str!("../../assets/main.dev.css"),
-    );
+    let style = format!("\n{}", include_str!("../../assets/main.dev.css"),);
 
     rsx! {
         style { "{style}" }
--- src/store/mod.rs
+++ src/store/mod.rs
@@ -10,13 +10,14 @@ use indexed_db_futures::{IdbDatabase, IdbKeyPath, IdbQuerySource, IdbVersionChan
 use serde_wasm_bindgen::{from_value, to_value};
 use std::future::IntoFuture;
 use std::sync::Arc;
-use subscription::{CustomSub, RelaySet};
+use subscription::{CustomSub, RelaySet, SubNames};
 pub use user::{AccountType, User};
 use wasm_bindgen::JsValue;
 use web_sys::IdbIndexParameters;
 
 const CURRENT_DB_VERSION: u32 = 2;
 const RELAY_SET_CF: &str = "relay-set";
+const SUB_NAME_LIST: &str = "sub-name-list";
 const CUSTOM_SUB_CF: &str = "custom-sub";
 const USER_CF: &str = "user";
 
@@ -81,6 +82,24 @@ impl CBWebDatabase {
                     }
 
                     {
+                        //init sub-name-list names store
+                        let mut create_store_params = IdbObjectStoreParameters::new();
+                        let key_path = IdbKeyPath::str("name");
+                        create_store_params.key_path(Some(&key_path));
+                        let relay_set_store = evt
+                            .db()
+                            .create_object_store_with_params(SUB_NAME_LIST, &create_store_params)
+                            .unwrap();
+                        relay_set_store
+                            .create_index_with_params(
+                                "name",
+                                &key_path,
+                                IdbIndexParameters::new().unique(true),
+                            )
+                            .unwrap();
+                    }
+
+                    {
                         //init custom-sub store
                         let mut create_store_params = IdbObjectStoreParameters::new();
                         let key_path = IdbKeyPath::str("name");
@@ -101,7 +120,6 @@ impl CBWebDatabase {
                 Ok(())
             },
         ));
-
 
         self.db.close();
 
@@ -142,6 +160,7 @@ impl CBWebDatabase {
             .transaction_on_one_with_mode(RELAY_SET_CF, IdbTransactionMode::Readwrite)?;
 
         let store = tx.object_store(RELAY_SET_CF)?;
+        store.delete(&to_value(&relay_set.name.clone()).map_err(CBwebDatabaseError::DeserializationError)?)?;
         let value = to_value(&relay_set).map_err(CBwebDatabaseError::DeserializationError)?;
         store.add_val(&value)?;
 
@@ -168,12 +187,46 @@ impl CBWebDatabase {
         }
     }
 
+    pub async fn save_sub_name_list(&self, names: SubNames) -> Result<(), CBwebDatabaseError> {
+        let tx = self
+            .db
+            .transaction_on_one_with_mode(SUB_NAME_LIST, IdbTransactionMode::Readwrite)?;
+
+        let store = tx.object_store(SUB_NAME_LIST)?;
+        store.delete(&to_value(&names.name.clone()).map_err(CBwebDatabaseError::DeserializationError)?)?;
+        let value = to_value(&names).map_err(CBwebDatabaseError::DeserializationError)?;
+        store.add_val(&value)?;
+
+        tx.await.into_result()?;
+        Ok(())
+    }
+
+    pub async fn get_sub_name_list(&self) -> Result<SubNames, CBwebDatabaseError> {
+        let tx = self
+            .db
+            .transaction_on_one_with_mode(SUB_NAME_LIST, IdbTransactionMode::Readonly)?;
+
+        let store = tx.object_store(SUB_NAME_LIST)?;
+        let key = JsValue::from_str("SUBSCRIPTION_LIST");
+        match store.get(&key)?.await? {
+            Some(value) => match from_value::<SubNames>(value.clone()) {
+                Ok(name_list) => Ok(name_list),
+                Err(e) => {
+                    tracing::error!("Error deserializing Vec<String>: {:?}", e);
+                    Err(CBwebDatabaseError::DeserializationError(e))
+                }
+            },
+            None => Err(CBwebDatabaseError::NotFound),
+        }
+    }
+
     pub async fn save_custom_sub(&self, custom_sub: CustomSub) -> Result<(), CBwebDatabaseError> {
         let tx = self
             .db
             .transaction_on_one_with_mode(CUSTOM_SUB_CF, IdbTransactionMode::Readwrite)?;
 
         let store = tx.object_store(CUSTOM_SUB_CF)?;
+        store.delete(&to_value(&custom_sub.name.clone()).map_err(CBwebDatabaseError::DeserializationError)?)?;
         let value = to_value(&custom_sub).map_err(CBwebDatabaseError::DeserializationError)?;
         store.add_val(&value)?;
 
@@ -206,6 +259,7 @@ impl CBWebDatabase {
             .transaction_on_one_with_mode(USER_CF, IdbTransactionMode::Readwrite)?;
 
         let store = tx.object_store(USER_CF)?;
+        store.delete(&to_value(&user.name.clone()).map_err(CBwebDatabaseError::DeserializationError)?)?;
         let value = to_value(&user).map_err(CBwebDatabaseError::DeserializationError)?;
         store.add_val(&value)?;
 
