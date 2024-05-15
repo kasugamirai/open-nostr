703061a921f1fa0e97e62ab37ccb8afe213859ea
ahriknow <ahriknow@ahriknow.com>
2024-05-15 14:56:41 +0800

    feat: capybastr

--- src/views/note_detail/mod.rs
+++ src/views/note_detail/mod.rs
@@ -1,38 +1,11 @@
-use std::time::Duration;
+use dioxus::prelude::*;
+use nostr_sdk::EventId;
 
-use dioxus::{events, prelude::*};
-use nostr_sdk::{Alphabet, Event, EventId, Filter, JsonUtil, Kind, SingleLetterTag};
-use serde::Serialize;
-
-use crate::{
-    nostr::{
-        fetch::{get_event_by_id, get_replies},
-        multiclient::MultiClient,
-        note::{DisplayOrder, ReplyTrees, TextNote},
-    },
-    views::note_list::note::{Note, NoteData},
+use crate::nostr::{
+    fetch::{get_event_by_id, get_replies},
+    multiclient::MultiClient,
+    note::ReplyTrees,
 };
-
-#[derive(Debug, Clone, Serialize)]
-struct NoteTree {
-    content: String,
-    children: Vec<NoteTree>,
-    event: Event,
-}
-
-fn get_notetree(event: &Event) -> Vec<NoteTree> {
-    vec![NoteTree {
-        content: event.content.to_string(),
-        children: vec![],
-        event: event.clone(),
-    }]
-}
-
-impl PartialEq for NoteTree {
-    fn eq(&self, other: &Self) -> bool {
-        self.content == other.content
-    }
-}
 
 #[component]
 pub fn NoteDetail(sub: String, id: String) -> Element {
@@ -41,13 +14,10 @@ pub fn NoteDetail(sub: String, id: String) -> Element {
     let sub_name = use_signal(|| sub.clone());
     let event_id = use_signal(|| id.clone());
 
-    let mut count = use_signal(|| 0);
-
     let mut replytree = use_signal(|| ReplyTrees::default());
-    let on_mounted = move |_| {
+    let _ = use_resource(move || async move {
         spawn(async move {
             let clients = multiclient();
-            tracing::info!("clients: {:?}", clients);
             let client = clients.get(&sub_name.read()).unwrap();
 
             match get_event_by_id(&client, &EventId::from_hex(&event_id()).unwrap(), None).await {
@@ -71,7 +41,7 @@ pub fn NoteDetail(sub: String, id: String) -> Element {
                 }
             }
         });
-    };
+    });
 
     rsx! {
         div {
@@ -84,11 +54,7 @@ pub fn NoteDetail(sub: String, id: String) -> Element {
                 }
             }
             div {
-                style: "width: 300px;",
-                button {
-                    onclick: on_mounted,
-                    "Get Event"
-                }
+                style: "width: 500px;",
             }
         }
     }
@@ -97,7 +63,7 @@ pub fn NoteDetail(sub: String, id: String) -> Element {
 #[component]
 fn Layer(sub_name: String, event_id: String) -> Element {
     let multiclient = use_context::<Signal<MultiClient>>();
-    let mut element = use_signal(|| rsx! { div {} });
+    let mut element = use_signal(|| rsx! { div { "Loading..." } });
     let sub_name = use_signal(|| sub_name.clone());
     let event_id = use_signal(|| event_id.clone());
     let _ = use_resource(move || async move {
@@ -105,18 +71,28 @@ fn Layer(sub_name: String, event_id: String) -> Element {
         let client = clients.get(&sub_name()).unwrap();
         match get_event_by_id(&client, &EventId::from_hex(&event_id()).unwrap(), None).await {
             Ok(Some(event)) => {
-                match get_replies(&client, EventId::from_hex(&event_id()).unwrap(), None).await {
-                    Ok(replies) => {
-                        element.set(rsx! {
-                            div {
-                                "123"
+                let replies =
+                    match get_replies(&client, EventId::from_hex(&event_id()).unwrap(), None).await
+                    {
+                        Ok(replies) => replies,
+                        Err(e) => {
+                            tracing::error!("error: {:?}", e);
+                            vec![]
+                        }
+                    };
+                element.set(rsx! {
+                    div {
+                        "{event.content}"
+                    }
+                    div {
+                        for e in replies {
+                            Layer {
+                                sub_name: sub_name.read().clone(),
+                                event_id: e.id.to_string(),
                             }
-                        });
+                        }
                     }
-                    Err(e) => {
-                        tracing::error!("error: {:?}", e);
-                    }
-                }
+                });
             }
             Ok(None) => {
                 tracing::error!("event not found");
