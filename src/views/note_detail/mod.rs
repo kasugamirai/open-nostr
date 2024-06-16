a8e65ef881001bc82428a26fb00cd9e5a67c66a6
LiZhong <LZ020316@yeah.net>
2024-06-17 03:23:17 +0800

    chore: remove unused code

--- src/views/note_detail/mod.rs
+++ src/views/note_detail/mod.rs
@@ -135,48 +135,6 @@ pub fn NoteDetail(sub: String, root_id: String, note_id: String) -> Element {
         },
     ));
 
-    // use_effect(use_reactive(
-    //     (&tree(), &highlight_note_id()),
-    //     move |(newest_tree, newest_highlight_event_id)| {
-    //         render_notes.write().clear();
-    //         tracing::info!("newest_highlight_event_id: {:?}", newest_highlight_event_id);
-    //         if let Some(newest_tree) = newest_tree {
-    //             let highlight_event_id =
-    //                 &EventId::from_hex(newest_highlight_event_id.clone()).unwrap();
-    //             let highlight_note = newest_tree.get_note_by_id(highlight_event_id);
-
-    //             if let Some(highlight_note) = highlight_note {
-    //                 pubkey_str.set(highlight_note.inner.author().to_string().clone());
-    //                 // tracing::info!("newest_highlight_event_id: {:?}", newest_highlight_event_id);
-    //                 // group by highlight_note and find the longest chain
-    //                 let replies = newest_tree.get_replies(&highlight_note.inner.id, None);
-    //                 let chain = find_longest_chain(&newest_tree, highlight_note);
-    //                 let chain: Vec<Event> =
-    //                     chain.into_iter().map(|note| note.clone().inner).collect();
-    //                 let other_replies = replies
-    //                     .into_iter()
-    //                     .flat_map(|note| {
-    //                         let res = find_longest_chain(&newest_tree, note);
-    //                         res.into_iter()
-    //                             .map(|note| note.clone().inner)
-    //                             .collect::<Vec<Event>>()
-    //                     })
-    //                     .collect::<Vec<Event>>();
-    //                 let ancestors = newest_tree.get_ancestors(&highlight_note.inner.id);
-    //                 let mut all_replies = Vec::<Event>::new();
-    //                 all_replies.extend(ancestors.into_iter().map(|note| note.clone().inner));
-    //                 all_replies.extend(chain);
-    //                 // tracing::info!("chain and ancestors: ---------------------- {:?}", all_replies);
-    //                 // tracing::info!("other_replies: ---------------------- {:?}", other_replies);
-    //                 all_replies.extend(other_replies);
-    //                 // all_replies.de
-    //                 all_replies = vec_unique(all_replies, |e| e.id);
-
-    //                 render_notes.write().accept(all_replies);
-    //             }
-    //         }
-    //     },
-    // ));
     rsx! {
         div {
             class: "note-detail-mode-box",
